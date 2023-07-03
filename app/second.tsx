37b2a0553625e17fb8616bb89bedf5338ccbd09f
Sandeep-morya <saabmaurya@gmail.com>
2023-07-03 19:13:44 +0530

    project setuped

---
+++ app/second.tsx
@@ -0,0 +1,18 @@
+﻿import { View, Text, Button } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context"; 
+import { Link } from "expo-router";
+
+export default function Second() {
+	return (
+		<View>
+			<Text>Second</Text>
+			<Link href={"/"} asChild>
+				<Button title="Go To Home" />
+			</Link>
+			<Link href={"/third"} asChild>
+				<Button title="Go To Third Screen" />
+			</Link>
+		</View>
+	);
+}
